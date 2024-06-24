ab39ef5cc84b2ae8935d09d338f918a5e3dad174
Max Khotsin <slimgos22@gmail.com>
2024-06-24 16:21:07 +0200

    upload/ commit to pull original branch

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,8 +1,45 @@
+import { Box, Container, Divider, Link, Typography } from '@mui/material';
+
 export const MainPage = () => {
   return (
-    <div>
-      <p>First item</p>
-      <p>Second item</p>
-    </div>
-  )
-}
+    <Container
+      maxWidth="lg"
+      disableGutters
+      sx={{
+        marginTop: '10px',
+        border: '1px solid black',
+        borderRadius: '5px',
+        display: 'flex',
+      }}
+    >
+      <Box sx={{ background: '#F5F5F5', width: '75%', padding: '10px' }}>
+        <Typography
+          variant="h1"
+          sx={{ fontSize: '48px', fontWeight: '500', maxWidth: 'fitContent' }}
+        >
+          MAKSIM KHOTSIN (Frontend Developer)
+        </Typography>
+        <Divider orientation="horizontal" />
+
+        <Typography sx={{ marginTop: '10px', marginBottom: '10px' }}>
+          Digital CV with greater details than in PDF.
+        </Typography>
+        <Divider orientation="horizontal" />
+
+        <Link
+          variant="h4"
+          underline="hover"
+          sx={{ cursor: 'pointer', color: '#0027A5' }}
+        >
+          Summary
+        </Link>
+        <Divider
+          orientation="horizontal"
+          sx={{ border: '1px solid rgba(0, 0, 0, 0.5)' }}
+        />
+      </Box>
+
+      <Box sx={{ background: '#13376E', width: '25%' }}>123</Box>
+    </Container>
+  );
+};
